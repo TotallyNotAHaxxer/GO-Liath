b750c33c10de0b6f4aefeb52cc5ab103274c41ee
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2021-10-28 23:52:00 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+go get github.com/PuerkitoBio/goquery ; go get github.com/atotto/clipboard ; go get github.com/logrusorgru/aurora ; go get github.com/steelx/extractlinks ; go get golang.org/x/net/html ; pip3 install bs4 ; pip install requests ; pip3 install colorama ; pip install colorama ; pip install urllib3
